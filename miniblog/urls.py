b567aa92cb58084d5719762e552c23f58576cee8
Neralis <coopmaster333@gmail.com>
2025-01-29 10:21:08 +0300

    Скелет

---
+++ miniblog/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', admin.site.urls),
+    path('', RedirectView.as_view(url='/home/', permanent=True)),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
